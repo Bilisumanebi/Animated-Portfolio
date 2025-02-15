cfe4e34448278cebb725acfc95c172ce07df1c08
Bilisumanebi <hbilisuma@yahoo.com>
2025-02-15 11:11:55 -0600

    final touch

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const video1 = document.getElementById("projectVideo1");
 const video2 = document.getElementById("projectVideo2");
 const video3 = document.getElementById("projectVideo3");
+const hoverSign = document.querySelector(".hover-sign");
+
+// sidebar elements
+const sideBar = document.querySelector(".sidebar");
+const menu = document.querySelector(".menu-icon");
+const close = document.querySelector(".close-icon");
+
+
+
 
 const videoList = [video1, video2, video3];
 
@@ -16,4 +25,16 @@ videoList.forEach((video) => {
   video.addEventListener("mouseout", () => {
     video.pause();
   });
+});
+
+// Open Sidebar
+menu.addEventListener("click", () => {
+  sideBar.classList.remove("close-sidebar"); // Remove closing animation
+  sideBar.classList.add("open-sidebar"); // Add opening animation
+});
+
+// Close Sidebar
+close.addEventListener("click", () => {
+  sideBar.classList.remove("open-sidebar"); // Remove opening animation
+  sideBar.classList.add("close-sidebar"); // Add closing animation
 });
